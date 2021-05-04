e1f639b14912cfdc939aff529ad113651654ab1e
MathisDelrue51 <mathis.delrue@gmail.com>
2021-05-04 17:01:40 +0200

    import fake data

--- back/migrations/deploy/init.sql
+++ back/migrations/deploy/init.sql
@@ -2,7 +2,7 @@
 
 BEGIN;
 
-CREATE TABLE user (
+CREATE TABLE client (
     id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     email text NOT NULL UNIQUE,
     password text NOT NULL,
@@ -19,13 +19,13 @@ CREATE TABLE curioset (
     title text NOT NULL,
     description text NOT NULL,
     address text NOT NULL,
-    posx float NOT NULL,
-    posy float NULL NULL,
+    latitude float NOT NULL,
+    longitude float NULL NULL,
     website text,
     agenda text NOT NULL,
     price float,
-    user_id int REFERENCES user(id),
-    category_id int REFERENCES category(id)
+    client_id int NOT NULL REFERENCES client(id),
+    category_id int NOT NULL REFERENCES category(id)
 );
 
 COMMIT;
