d42604e75dbe61aafe1d0abd5eeac8e37f755467
marion-vgv <marion.vangaeveren@gmail.com>
2021-05-05 14:44:42 +0200

    loginForm component created

---
+++ front/src/components/LoginForm/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Field from './Field';
+import SubmitButton from './SubmitButton';
+
+import './styles.css';
+
+const LoginForm = () => (
+    <div className="login-form">
+
+    </div>
+        
+);
+
+
+
+export default LoginForm;
