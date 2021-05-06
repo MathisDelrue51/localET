ecde2f1ea5e21f0209f5960d149248c9a003dff9
AliceMsx <alice.masseaux01@gmail.com>
2021-05-06 10:14:45 +0200

    managed to move map container in map component

--- front/src/components/App/index.js
+++ front/src/components/App/index.js
@@ -7,39 +7,13 @@ import {
 // == Import
 import './styles.scss';
 import Map from "src/components/Map";
-
 import data from "../../data.js";
-// const marker = {
-//   id: 1,
-//   title: 'Best of van gogh',
-//   description: 'une présentation de ses meilleures oeuvres',
-//   address: '12 Rue du Croissant, Nantes, France',
-//   latitude: 47.23737335205078,
-//   longitude: -1.5248912572860718,
-//   category: "Expos",
-// };
 
 // == Component
 const App = () => (
   <div className="app">
     <div>Ici, le header</div>
-    <MapContainer center={[47.23737335205078, -1.5248912572860718,]} zoom={13} scrollWheelZoom={false} id="mapid">
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      { data.map(elmt => (
-        <Marker key={elmt.id} position={[elmt.latitude, elmt.longitude]}>
-        <Popup>
-        <div className="popup"> 
-          <div className="title">{elmt.title}</div> <br/> {elmt.address} <br /> {elmt.description}
-        </div>
-        </Popup>
-      </Marker>
-      ))
-      }
-
-    </MapContainer>
+    <Map />
     <div>Ici, le footer</div>
   </div>
 );
--- front/src/components/Map/index.js
+++ front/src/components/Map/index.js
@@ -1,5 +1,6 @@
 // == Import npm
 import React from 'react';
+import PropTypes from 'prop-types';
 
 // == Import
 import './styles.scss';
@@ -8,21 +9,28 @@ import {
   MapContainer, TileLayer, Marker, Popup,
 } from 'react-leaflet';
 
+import data from "../../data.js";
+
 // == Component
-const Map = (list) => (
+const Map = () => (
   <div className="map">
 
-  {console.log(list)}
-   <MapContainer center={[list.latitude, list.longitude]} zoom={13} scrollWheelZoom={false} id="mapid">
+  {console.log(data)}
+   <MapContainer center={[47.23737335205078, -1.5248912572860718]} zoom={13} scrollWheelZoom={false} id="mapid">
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-        <Marker position={[list.latitude, list.longitude]}>
-        <Popup className="popup">
-          {list.address} <br /> {list.description}
+       { data.map(elmt => (
+        <Marker key={elmt.id} position={[elmt.latitude, elmt.longitude]}>
+        <Popup>
+        <div className="popup"> 
+          <div className="title">{elmt.title}</div> <br/> {elmt.address} <br /> {elmt.description}
+        </div>
         </Popup>
       </Marker>
+      ))
+      }
 
     </MapContainer> 
   </div>
