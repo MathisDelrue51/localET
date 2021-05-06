e2a9b418f079339ff10e87afd324c7c99c9f6c18
AliceMsx <alice.masseaux01@gmail.com>
2021-05-06 13:26:41 +0200

    react search removed

--- front/src/components/Map/index.js
+++ front/src/components/Map/index.js
@@ -13,9 +13,8 @@ import data from "../../data.js";
 
 // == Component
 const Map = () => (
+  
   <div className="map">
-
-  {console.log(data)}
    <MapContainer center={[47.23737335205078, -1.5248912572860718]} zoom={13} scrollWheelZoom={false} id="mapid">
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
