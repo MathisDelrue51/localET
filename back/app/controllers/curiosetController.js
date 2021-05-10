bb97f4fef6da047ee5c4c1067153aa84a2890ec9
MathisDelrue51 <mathis.delrue@gmail.com>
2021-05-10 16:34:32 +0200

    verify token to access corresponding profile page

--- back/app/controllers/curiosetController.js
+++ back/app/controllers/curiosetController.js
@@ -5,9 +5,33 @@ const curiosetController = {
     //GET /
     allCuriosets: async (req, res) => {
 
-        const curiosets = await Curioset.findAll();
+        try {
+            const curiosets = await Curioset.findAll();
 
-        res.status(200).json(curiosets);
+            res.status(200).json(curiosets);
+        } catch (err) {
+            res.status(500).json(err.message);
+        }
+
+    },
+
+    //GET /curioset/:id
+    oneCuriosetById: async (req, res) => {
+        try {
+            const curiosetId = req.params.id;
+
+            const curioset = await Curioset.findOne(curiosetId);
+
+            //This verification is a security. If the id doesn't correspond to one in the db, it should throw an error in the model.
+            if (curioset) {
+                res.status(200).json(curioset);
+            } else {
+                throw new Error('Cant find curioset with id ' + curiosetId)                
+            }
+
+        } catch (err) {
+            res.status(404).json(err.message);
+        }
     },
 
     //POST /curioset
@@ -20,7 +44,7 @@ const curiosetController = {
             await newCurioset.save();
 
             res.status(201).json(newCurioset);
-        } catch (err) {            
+        } catch (err) {
             console.log(err)
             res.status(500).json(err.message);
         }
