3cac3647de41c5173346ba7fb83a1967f8c85b06
MathisDelrue51 <mathis.delrue@gmail.com>
2021-05-12 15:03:25 +0200

    small modif

--- back/app/middlewares/authenticateToken.js
+++ back/app/middlewares/authenticateToken.js
@@ -2,8 +2,8 @@ const jwt = require('jsonwebtoken');
 
 function authenticateToken(req, res, next) {    
     
-    const authHeader = req.headers['authorization']
-    const token = authHeader && authHeader.split(' ')[1]
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
   
     if (token == null){
         return res.status(401).send('No user connected');
