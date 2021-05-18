6f23b884e710d0542bff9ca11f8cf422701f527b
MathisDelrue51 <mathis.delrue@gmail.com>
2021-05-18 23:12:53 +0200

    v2 profile page

--- front/src/components/ProfilePage/index.js
+++ front/src/components/ProfilePage/index.js
@@ -4,14 +4,14 @@ import './styles.scss';
 
 const ProfilePage = ({ email, pseudo, profileList }) => (
   <div className="container">
-    <div>
+    <div className="componentTitle">
       Mon Profil LocalET
     </div>
     <div className="profileInformation">
       <div className="pseudo">
         Pseudo: {pseudo}
       </div>
-      <div className="identifiants">
+      <div className="logins">
         <div>
           E-mail: {email}
         </div>
@@ -20,24 +20,34 @@ const ProfilePage = ({ email, pseudo, profileList }) => (
         </div>
       </div>
     </div>
-    {
+    <div className="myCuriosets">
+      <div className="componentTitle">
+        Mes curiosETs
+      </div>
+      {
       profileList.map((element) => (
         <div className="curioset" key={element.id}>
-          <div className="information">
-            <div className="row-1">
+          <div className="row-1">
+            <div className="title">
               {element.title}
             </div>
-            <div className="row-2">
+            <div className="category">
+              {element.type}
+            </div>
+          </div>
+          <div className="row-2">
+            <div className="curiosetInformation">
               <div className="place">{element.address}</div>
               <div className="agenda">{element.agenda}</div>
             </div>
-          </div>
-          <div className="accessButton">
-            <button className="access__button" type="button">Accéder à la page</button>
+            <div className="accessButton">
+              <button className="theButton" type="button">Voir</button>
+            </div>
           </div>
         </div>
       ))
     }
+    </div>
   </div>
 );
 
