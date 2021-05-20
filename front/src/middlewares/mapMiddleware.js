f9a6a1441a55f17de2bdc4840b289c24c4fc0ed6
Idrissi-55 <idrissialamiyassine@gmail.com>
2021-05-20 14:39:30 +0200

    router /home

--- front/src/middlewares/mapMiddleware.js
+++ front/src/middlewares/mapMiddleware.js
@@ -19,7 +19,7 @@ export default (store) => (next) => (action) => {
       next(action);
       axios({
         method: 'get',
-        url: `${SERVER_URL}/`,
+        url: `${SERVER_URL}/home`,
       })
         .then((res) => { 
           console.log('je reçois ça du back', res);
