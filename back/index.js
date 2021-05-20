c6064c703ba7b0c349fc7a30c5205dab25e32515
Idrissi-55 <idrissialamiyassine@gmail.com>
2021-05-20 10:31:55 +0200

    added allowance

--- back/index.js
+++ back/index.js
@@ -35,6 +35,14 @@ app.use(express.static(path.join(__dirname,'../front/dist')));
 
 app.use('/api', router);
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 app.use(cors());
 
 app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
