73bd9d77f65eb2692cd989fa735af24cc537794c
Idrissi-55 <idrissialamiyassine@gmail.com>
2021-05-20 15:28:45 +0200

    modified router

--- back/index.js
+++ back/index.js
@@ -22,11 +22,6 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname,'../front/dist')));   
 
-app.get('/*', (_,res) => {
-        res.sendFile(path.join(__dirname,'../front/dist/index.html'));
-});
-
-
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);
@@ -34,7 +29,6 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
 });
-
 
 //We will put connected users in a session
 app.use( session({
